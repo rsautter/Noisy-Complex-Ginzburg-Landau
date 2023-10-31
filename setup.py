95b3e9780def3772bb6b80209b5914659474c25e
Rubens <rubens.sautter@gmail.com>
2023-10-31 10:03:26 -0300

    included packing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='NCGL',
+    version='0.1',
+    url='https://github.com/rsautter/Noisy-Complex-Ginzburg-Landau',
+    keywords='Colored-Noise Ginzburg Landau Ginzburg Landau 2D 1D 3D 2D+1 1D+1 3D+1',
+    description=u'',
+    packages=find_packages()
+)
